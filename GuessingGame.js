2703071c84ea9632f5242fe3b89e58ff92af30ad
ohagert1 <ohagerty@gmail.com>
2017-10-19 15:25:11 -0400

    Finished Product

--- GuessingGame.js
+++ GuessingGame.js
@@ -2,24 +2,27 @@ function Game() {
 	this.playersGuess = null;
 	this.pastGuesses = [];
 	this.winningNumber = generateWinningNumber();
+	this.over = false;
 }
 
 Game.prototype.playersGuessSubmission = function(playerInput) {
-	if(playerInput < 1 || playerInput > 100 || !(typeof playerInput === 'number')) {
-		throw 'That is an invalid guess.'
+	if(playerInput < 1 || playerInput > 100 || !Number.isInteger(parseInt(playerInput))) {
+		return 'That is an invalid guess.'
 	}
-	this.playersGuess = playerInput;
+	this.playersGuess = parseInt(playerInput);
 	return this.checkGuess();
 }
 
 Game.prototype.checkGuess = function() {
 	var diff = this.difference();
+	if(this.pastGuesses.indexOf(this.playersGuess) !== -1) {
+		return "You have already guessed that number.";
+	}
+	this.pastGuesses.push(this.playersGuess)
 	if(this.playersGuess == this.winningNumber) {
 		return "You Win!";
-	} else if(this.pastGuesses.indexOf(this.playersGuess) !== -1) {
-		return "You have already guessed that number.";
 	} else {
-		this.pastGuesses.push(this.playersGuess)
+		this.playersGuess = null;
 		if(this.pastGuesses.length === 5) {
 			return "You Lose.";
 		} else if(diff < 10) {
@@ -43,7 +46,11 @@ Game.prototype.isLower = function() {
 }
 
 Game.prototype.provideHint = function() {
-	return shuffle([generateWinningNumber(), generateWinningNumber(), this.winningNumber]);
+	if(this.pastGuesses.length > 2) {
+		return "It's one of the following: " + shuffle([generateWinningNumber(), generateWinningNumber(), this.winningNumber]);
+	} else {
+		return "You haven't even tried yet"
+	}
 }
 
 
@@ -64,4 +71,55 @@ function shuffle(arr) {
 	}
 	return arr;
 }
+
+$(document).ready(function() {
+	function resetGame() {
+		newGame = new Game();
+		$('.guess').text('-');
+		$('#player-input').val('');
+		$('#title').text('Guessing Game');
+		$('#subtitle').text('Guess any number between 1 and 100');
+	}
+	function submit(){
+		if(!newGame.over) {
+			var newGuess = $('#player-input').val();
+			var result = newGame.playersGuessSubmission(newGuess);
+			if(result != "That is an invalid guess." && result != "You have already guessed that number.") {
+				if(newGame.pastGuesses.length <= 5) {
+					$('.guess').eq(newGame.pastGuesses.length - 1).text(newGuess);
+				}
+				$('#title').text(result);
+				if(result === 'You Win!' || result === 'You Lose.') {
+					newGame.over = true;
+				}
+			}
+			$('#player-input').val('');
+		}
+	}
+	var newGame;
+	resetGame();
+	$('#submit').click(submit);
+	$('body').on('keydown', function(key) {
+		if(key.keyCode == 13) {
+			submit();
+		}
+	})
+	$('#reset').click(function() {
+		resetGame();
+	})
+	$('body').on('keydown', function(key) {
+		if(key.keyCode == 72) {
+			alert(newGame.winningNumber);
+		}
+	})
+	$(':button').on('mouseenter', function() {
+		$(this).animate({'top' : '-=2px'}, 100);
+	});
+	$(':button').on('mouseleave', function() {
+		$(this).animate({'top' : '+=2px'}, 100);
+	});
+	$('#hint').click(function(){
+		alert(newGame.provideHint());
+	})
+});
 
